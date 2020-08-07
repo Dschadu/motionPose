cc2df064af134d62ab965a9779d41a8950d9fb59
Martin <darki.klein@gmail.com>
2020-08-07 18:08:37 +0200

    Code refactoring. Added OVRMC connection

--- motionPose/MotionPoseController.cpp
+++ motionPose/MotionPoseController.cpp
@@ -1,28 +1,26 @@
 #include "MotionPoseController.h"
-#include "driverlog.h"
+#include "third-party/easylogging++/easylogging++.h"
 
-//class CMotionPoseControllerDriver : public vr::ITrackedDeviceServerDriver
+INITIALIZE_EASYLOGGINGPP
+
 namespace driver
 {
 	CMotionPoseControllerDriver::CMotionPoseControllerDriver()
 	{
+		init_logging();		
+
 		m_unObjectId = vr::k_unTrackedDeviceIndexInvalid;
 		m_ulPropertyContainer = vr::k_ulInvalidPropertyContainer;
 
-		m_sSerialNumber = "MotionPoseVirtualController 0.1";
-
+		m_sSerialNumber = "MotionPoseVirtualController 0.2.0";
 		m_sModelNumber = "MotionPoseVirtualController";
-		rigPitch = 0;
-		rigRoll = 0;
-		rigYaw = 0;
-		rigHeave = 0;
-		rigSurge = 0;
-		rigSway = 0;
-		yawComp = 0;
-		pose = { 0 };
-		pose.qWorldFromDriverRotation = HmdQuaternion_Init(1, 0, 0, 0);
-		pose.qDriverFromHeadRotation = HmdQuaternion_Init(1, 0, 0, 0);
-		pose.qRotation = HmdQuaternion_Init(1, 0, 0, 0);
+
+		rigYawOffset = 0;
+
+		pose.poseTimeOffset = 0;
+		pose.qWorldFromDriverRotation = { 1, 0, 0, 0 };
+		pose.qDriverFromHeadRotation = { 1, 0, 0, 0 };
+		pose.qRotation = { 1, 0, 0, 0 };
 		pose.vecDriverFromHeadTranslation[0] = 0;
 		pose.vecDriverFromHeadTranslation[1] = 0;
 		pose.vecDriverFromHeadTranslation[2] = 0;
@@ -32,7 +30,8 @@ namespace driver
 		pose.vecPosition[0] = 0;
 		pose.vecPosition[1] = 0;
 		pose.vecPosition[2] = 0;
-		rigPos = { 0 };
+
+		rigOffset = { 0, 0, 0 };
 	}
 
 	CMotionPoseControllerDriver::~CMotionPoseControllerDriver()
@@ -43,7 +42,7 @@ namespace driver
 
 	vr::EVRInitError CMotionPoseControllerDriver::Activate(vr::TrackedDeviceIndex_t unObjectId)
 	{
-		DriverLog("Activate motionPoseController\n");
+		LOG(INFO) << "Activate motionPoseController\n";
 
 		m_unObjectId = unObjectId;
 		m_ulPropertyContainer = vr::VRProperties()->TrackedDeviceToPropertyContainer(m_unObjectId);
@@ -60,37 +59,100 @@ namespace driver
 		return vr::VRInitError_None;
 	}
 
-	void CMotionPoseControllerDriver::initalizeMoverMmf()
+	void CMotionPoseControllerDriver::init_logging()
 	{
-		moverConnected = false;
+		el::Loggers::addFlag(el::LoggingFlag::DisableApplicationAbortOnFatalLog);
+		el::Configurations conf(logConfigFileName);
+		conf.parseFromText(logConfigDefault);
+		//conf.parseFromFile(logConfigFileName);
+		conf.setRemainingToDefault();
+		el::Loggers::reconfigureAllLoggers(conf);
+
+		LOG(INFO) << "|========================================================================================|";
+		LOG(INFO) << "motionPose dll loaded...";
+		LOG(TRACE) << "Trace messages enabled.";
+		LOG(DEBUG) << "Debug messages enabled.";
+	}
+
+	bool CMotionPoseControllerDriver::openMmf(HANDLE& MapFile, char* mmfFile, LPCWSTR szName, int BufferSize, bool& Connected)
+	{
+
+		MapFile = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, szName);
+
+		// return if MMF could not be opened
+		if (MapFile == NULL)
+		{
+			return false;
+		}
+
+		mmfFile = (char*)MapViewOfFile(MapFile,	FILE_MAP_READ, 0, 0, BufferSize);
+
+		// return if view could not be mapped
+		if (mmfFile == NULL)
+		{
+			CloseHandle(MapFile);
+			return false;
+		}
+
+		Connected = true;
+
+		return true;
+	}
+
+	bool CMotionPoseControllerDriver::initializeMoverMmf()
+	{
 		TCHAR szName[] = TEXT("Local\\motionRigPose");
 
-		hMapFile = OpenFileMapping(
-			FILE_MAP_ALL_ACCESS,   // read/write access
-			FALSE,                 // do not inherit the name
-			szName);               // name of mapping object
+		MapFile_Mover = OpenFileMapping(FILE_MAP_ALL_ACCESS,   // read / write access
+										FALSE,                 // do not inherit the name
+										szName);               // name of mapping object
 
-		if (hMapFile == NULL)
+		// return if MMF could not be opened
+		if (MapFile_Mover == NULL)
 		{
-			//			DriverLog("could not open mmf motionRigPose\n");
+			return false;
 		}
-		else
+
+		mmfFile_Mover = (char*)MapViewOfFile(MapFile_Mover,				// handle to map object
+											 FILE_MAP_READ,		// read / write permission
+											 0,
+											 0,
+											 BUF_SIZE);
+
+		// return if view could not be mapped
+		if (mmfFile_Mover == NULL)
 		{
-			//			DriverLog("successfully opened mmf motionRigPose\n");
-			mmfFile = (char*)MapViewOfFile(hMapFile,				// handle to map object
-										   FILE_MAP_ALL_ACCESS,		// read/write permission
-										   0,
-										   0,
-										   BUF_SIZE);
-			if (strncmp(mmfFile, "posedata", 8) == 0)
-			{
-				DriverLog("successfully receiving posedata\n");
-				moverConnected = true;
-			}
-			else
-			{
-				// DriverLog("no valid posedata\n");
-			}
+			CloseHandle(MapFile_Mover);
+			return false;
+		}
+		moverConnected = true;
+
+		return true;
+	}
+
+	void CMotionPoseControllerDriver::initializeOvrmcMmf()
+	{
+		TCHAR szName[] = TEXT("Local\\OVRMC_MMFv1");
+
+		MapFile_OVRMC = OpenFileMapping(FILE_MAP_ALL_ACCESS,   // read / write access
+										FALSE,                 // do not inherit the name
+										szName);               // name of mapping object
+
+		if (MapFile_OVRMC == NULL)
+		{
+			return;
+		}
+
+		mmfFile_OVRMC = (char*)MapViewOfFile(MapFile_OVRMC,				// handle to map object
+											 FILE_MAP_ALL_ACCESS,		// read / write permission
+											 0,
+											 0,
+											 4096);
+
+		if (mmfFile_Mover == NULL)
+		{
+			CloseHandle(MapFile_OVRMC);
+			return;
 		}
 	}
 
@@ -122,85 +184,84 @@ namespace driver
 
 	vr::DriverPose_t CMotionPoseControllerDriver::GetPose()
 	{
-		//		DriverLog("virtualPoseController::GetPose\n");
-
-		pose.poseIsValid = true;
-		pose.deviceIsConnected = true;
+		pose.poseIsValid = false;
+		pose.deviceIsConnected = moverConnected;
 		pose.shouldApplyHeadModel = false;
 		pose.willDriftInYaw = false;
 
-		vr::HmdQuaternion_t yaw = vrmath::quaternionFromRotationY(yawComp);
+		vr::HmdQuaternion_t yaw = vrmath::quaternionFromRotationY(rigYawOffset);
 
-		// Turn Right/Left
-		if ((GetAsyncKeyState('E') & 0x8000) != 0)			yawComp += 0.005;
-		if ((GetAsyncKeyState('R') & 0x8000) != 0)			yawComp -= 0.005;
+		// Create connection to OVRMC
+		if (!ovrmcConnected)
+		{
+			if (openMmf(MapFile_OVRMC, mmfFile_OVRMC, L"Local\\OVRMC_MMFv1", 4096, ovrmcConnected))
+			{
+				Data_OVRMC = (MMFstruct_OVRMC_v1*)mmfFile_OVRMC;
+			}			
+		}
+		else
+		{
+			// Get data from OVRMC
+			rigOffset = Data_OVRMC->Translation;
+			rigYawOffset = Data_OVRMC->Rotation.v[1];
+		}
+
+		/*// Turn Right/Left
+		if ((GetAsyncKeyState('E') & 0x8000) != 0)			rigYawOffset += 0.005;
+		if ((GetAsyncKeyState('R') & 0x8000) != 0)			rigYawOffset -= 0.005;
 
 		// Offset Left/Right
-		if ((GetAsyncKeyState('Q') & 0x8000) != 0)			rigPos = rigPos + vrmath::quaternionRotateVector(yaw, HmdVector3d_t_Init(-0.003, 0, 0));
-		if ((GetAsyncKeyState('W') & 0x8000) != 0)			rigPos = rigPos + vrmath::quaternionRotateVector(yaw, HmdVector3d_t_Init(0.003, 0, 0));
+		if ((GetAsyncKeyState('Q') & 0x8000) != 0)			rigOffset = rigOffset + vrmath::quaternionRotateVector(yaw, HmdVector3d_t_Init(-0.003, 0, 0));
+		if ((GetAsyncKeyState('W') & 0x8000) != 0)			rigOffset = rigOffset + vrmath::quaternionRotateVector(yaw, HmdVector3d_t_Init(0.003, 0, 0));
 
 		// Offset Up/Down
-		if ((GetAsyncKeyState('A') & 0x8000) != 0)			rigPos = rigPos + vrmath::quaternionRotateVector(yaw, HmdVector3d_t_Init(0, 0.003, 0));
-		if ((GetAsyncKeyState('S') & 0x8000) != 0)			rigPos = rigPos + vrmath::quaternionRotateVector(yaw, HmdVector3d_t_Init(0, -0.003, 0));
+		if ((GetAsyncKeyState('A') & 0x8000) != 0)			rigOffset = rigOffset + vrmath::quaternionRotateVector(yaw, HmdVector3d_t_Init(0, 0.003, 0));
+		if ((GetAsyncKeyState('S') & 0x8000) != 0)			rigOffset = rigOffset + vrmath::quaternionRotateVector(yaw, HmdVector3d_t_Init(0, -0.003, 0));
 
 		// Offset Forward/Backward
-		if ((GetAsyncKeyState('Y') & 0x8000) != 0)			rigPos = rigPos + vrmath::quaternionRotateVector(yaw, HmdVector3d_t_Init(0, 0, -0.003));
-		if ((GetAsyncKeyState('X') & 0x8000) != 0)			rigPos = rigPos + vrmath::quaternionRotateVector(yaw, HmdVector3d_t_Init(0, 0, 0.003));
+		if ((GetAsyncKeyState('Y') & 0x8000) != 0)			rigOffset = rigOffset + vrmath::quaternionRotateVector(yaw, HmdVector3d_t_Init(0, 0, -0.003));
+		if ((GetAsyncKeyState('X') & 0x8000) != 0)			rigOffset = rigOffset + vrmath::quaternionRotateVector(yaw, HmdVector3d_t_Init(0, 0, 0.003));
 
 		// reset
 		if ((GetAsyncKeyState('P') & 0x8000) != 0)
 		{
-			rigPos = { 0 };
-			yawComp = 0;
-		}
+			rigOffset = { 0 };
+			rigYawOffset = 0;
+		}*/
 
-		if (moverConnected && strncmp(mmfFile, "posedata", 8) == 0)
-		{
-			pose.result = vr::TrackingResult_Running_OK;
-			// <PoseSway><PoseSurge><PoseHeave><PoseYaw><PoseRoll><PosePitch>
-			double* poseData = (double*)mmfFile;
-			rigSway = poseData[1];
-			rigSurge = poseData[2];
-			rigHeave = poseData[3];
-			rigYaw = poseData[4];
-			rigRoll = poseData[5];
-			rigPitch = poseData[6];
-			/*			char buf[100];
-						sprintf_s(buf, "virtualPoseController::rigYaw: %lf\n", rigYaw);
-						DriverLog(buf);
-						sprintf_s(buf, "virtualPoseController::rigPitch: %lf\n", rigPitch);
-						DriverLog(buf);*/
-
-			vr::HmdVector3d_t rigTranslation = { 0 };
-			rigTranslation.v[0] = rigSway / 1000;
-			rigTranslation.v[1] = rigHeave / 1000;
-			rigTranslation.v[2] = -rigSurge / 1000;
-
-			pose.qRotation = vrmath::quaternionFromYawRollPitch(yawComp + rigYaw * 0.01745329251994329, -rigRoll * 0.01745329251994329, rigPitch * 0.01745329251994329);
-			vr::HmdVector3d_t rigVector = vrmath::quaternionRotateVector(pose.qRotation, rigTranslation) + rigPos;
-			pose.vecPosition[0] = rigVector.v[0];
-			pose.vecPosition[1] = rigVector.v[1];
-			pose.vecPosition[2] = rigVector.v[2];
-
-			/*			pose.qWorldFromDriverRotation = vrmath::quaternionFromYawPitchRoll(yawComp, 0, 0);
-						pose.vecWorldFromDriverTranslation[0] = rigX;
-						pose.vecWorldFromDriverTranslation[1] = rigY;
-						pose.vecWorldFromDriverTranslation[2] = rigZ;*/
-
-			return pose;
-		}
-		else
+		if (!moverConnected)
 		{
 			pose.result = vr::TrackingResult_Calibrating_InProgress;
-			pose.deviceIsConnected = false;
-			initalizeMoverMmf();
+			pose.qRotation = vrmath::quaternionFromYawRollPitch(rigYawOffset, 0, 0);
 
-			pose.qRotation = vrmath::quaternionFromYawRollPitch(yawComp, 0, 0);
-			vr::HmdVector3d_t rigVector = rigPos;
+			// Init was unsuccessful, return empty pose
+			if (!openMmf(MapFile_Mover, mmfFile_Mover, L"Local\\motionRigPose", 4096, moverConnected))
+			{
+				return pose;
+			}
 
-			return pose;
+			rigPose = (MMFstruct_Mover_v1*)mmfFile_Mover;
 		}
 
+		pose.poseIsValid = true;
+		pose.result = vr::TrackingResult_Running_OK;		
+
+		// Convert from mm to m
+		vr::HmdVector3d_t rigTranslation = { 0 };
+		rigTranslation.v[0] = rigPose->rigSway / (double)1000.0;
+		rigTranslation.v[1] = rigPose->rigHeave / (double)1000.0;
+		rigTranslation.v[2] = -rigPose->rigSurge / (double)1000.0;
+
+		// Create the rotation. Convert from degree to radian
+		pose.qRotation = vrmath::quaternionFromYawRollPitch(rigYawOffset + rigPose->rigYaw * 0.01745329251994329, -rigPose->rigRoll * 0.01745329251994329, rigPose->rigPitch * 0.01745329251994329);
+
+		// Create the translation (XYZ position in 3d space)
+		vr::HmdVector3d_t rigVector = vrmath::quaternionRotateVector(pose.qRotation, rigTranslation) + rigOffset;
+		pose.vecPosition[0] = rigVector.v[0];
+		pose.vecPosition[1] = rigVector.v[1];
+		pose.vecPosition[2] = rigVector.v[2];
+
+		return pose;
 	}
 
 	void CMotionPoseControllerDriver::RunFrame()
@@ -208,10 +269,9 @@ namespace driver
 		// Your driver would read whatever hardware state is associated with its input components and pass that
 		// in to UpdateBooleanComponent. This could happen in RunFrame or on a thread of your own that's reading USB
 		// state. There's no need to update input state unless it changes, but it doesn't do any harm to do so.
-		// DriverLog(" virtualPoseController::RunFrame\n");
 
 		// Collect events
-/*		vr::VREvent_t event;
+		/*vr::VREvent_t event;
 		while (vr::VRServerDriverHost()->PollNextEvent(&event, sizeof(event)))
 		{
 			if (event.trackedDeviceIndex == this->m_unObjectId && event.eventType == EVREventType::VREvent_PropertyChanged) {
